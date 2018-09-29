66c0a11c8cdc0a76a2cee0b8a6af632bde237e2f
Carmine <calicchiocarmine@gmail.com>
2018-09-29 09:31:33 +0100

    SW-W2-L2

---
+++ Week2/Lab1/DebugTwo1.java
@@ -0,0 +1,14 @@
+public class DebugTwo1
+{
+   public static void main(String[] args)
+   {
+      int oneInt = 315;
+      double oneDouble = 12.4;
+
+      System.out.print("The int is ");
+      System.out.println(oneInt);
+      System.out.print("The double is ");
+      System.out.println(oneDouble);
+
+   }
+}
