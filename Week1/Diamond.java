fc455d572d25de4a7cb1f8fe3752812cfb5647cb
Carmine <calicchiocarmine@gmail.com>
2018-09-29 09:25:39 +0100

    SD-W1-L1

---
+++ Week1/Diamond.java
@@ -0,0 +1,19 @@
+/*
+Diamond.java
+Carmine Calicchio
+20180919
+*/
+
+//Class Signature
+public class Diamond{
+    //Main Method
+  public static void main(String[] args) {
+    String diamond = "  *  \n"
+                   + " * * \n"
+                   + "* * *\n"
+                   + " * * \n"
+                   + "  *  \n";
+
+    System.out.println(diamond);
+  }
+}
