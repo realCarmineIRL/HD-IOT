ed89076f7155f6eaf18ad605ca6b305a6b16c6de
Carmine <calicchiocarmine@gmail.com>
2018-11-15 01:43:47 +0000

    Project-part2

--- SoftwareDevelopment/Project/LotteryApp.java
+++ SoftwareDevelopment/Project/LotteryApp.java
@@ -10,12 +10,20 @@ public class LotteryApp{
         
         List<Integer> lotteryLine;
         LotteryGenLine myLotteryGenLine;
-        myLotteryGenLine = new LotteryGenLine();
 
+        myLotteryGenLine = new LotteryGenLine();
         myLotteryGenLine.genLotteryLine();
 
         lotteryLine = myLotteryGenLine.getLotteryLine();
 
+        List<Integer> userLine = new ArrayList<Integer>(Arrays.asList(1,7,22,26,35,39));
+
+        List<Integer> matchedNumbers = userLine;
+
+        matchedNumbers.retainAll(lotteryLine);
+
         System.out.println(lotteryLine);
+
+        System.out.println(matchedNumbers);
     }
 }
