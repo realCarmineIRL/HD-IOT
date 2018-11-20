854ab52d6ca0b8848b4cbf35f8e2409c1653e423
Carmine <calicchiocarmine@gmail.com>
2018-11-20 23:31:00 +0000

    update2

--- SoftwareDevelopment/Project/LotteryApp.java
+++ SoftwareDevelopment/Project/LotteryApp.java
@@ -12,36 +12,41 @@ public class LotteryApp{
         List<Integer> lotteryLine;
         LotteryGenLine myLotteryGenLine;
         LotteryCheckUserLine myLotteryCheckUserLine;
+        String option;
 
         myLotteryGenLine = new LotteryGenLine();
-        myLotteryGenLine.genLotteryLine();
-
         myLotteryCheckUserLine = new LotteryCheckUserLine();
         
+        do {
 
-        lotteryLine = myLotteryGenLine.getLotteryLine();
+            myLotteryGenLine.genLotteryLine();
+            lotteryLine = myLotteryGenLine.getLotteryLine();
 
-        String[] userLines = new String[3];
+            String[] userLines = new String[3];
 
-        for (int i=0; i<userLines.length; i++) {
-            userLines[i] = JOptionPane.showInputDialog(null,"Please enter your line number " + (i+1) +":");
-        }
+            for (int i=0; i<userLines.length; i++) {
+                userLines[i] = JOptionPane.showInputDialog(null,"Please enter your line number " + (i+1) +":");
+            }
 
-        System.out.println(lotteryLine);
+            System.out.println(lotteryLine);
 
-        for(int i=0; i<userLines.length; i++) {
+            for(int i=0; i<userLines.length; i++) {
 
-            myLotteryCheckUserLine.setUserLine(userLines[i]);
+                myLotteryCheckUserLine.setUserLine(userLines[i]);
 
-            myLotteryCheckUserLine.compute();
-    
-            List<Integer> matchedNumbers = myLotteryCheckUserLine.getUserLine();
-    
-            matchedNumbers.retainAll(lotteryLine);
-    
-            System.out.println(userLines[i]);
-            System.out.println(matchedNumbers);
+                myLotteryCheckUserLine.compute();
+        
+                List<Integer> matchedNumbers = myLotteryCheckUserLine.getUserLine();
+        
+                matchedNumbers.retainAll(lotteryLine);
+        
+                System.out.println(userLines[i]);
+                System.out.println(matchedNumbers);
 
-        }
+            }
+
+            option = JOptionPane.showInputDialog(null,"Would you like to continue Y or N:");
+            
+        } while (option.equals("Y"));
     }
 }
